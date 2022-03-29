11d6950865e9095fb45aac62b434dcfb296d60f8
Aleks-Ada <aleks.todorov@ada.ac.uk>
2022-03-29 11:55:42 +0100

    Update requests to use AWS lambda backend.

--- aldify/src/components/MusicBadge.vue
+++ aldify/src/components/MusicBadge.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
-.flex
+.flex.w-72
   img.w-12.h-12.mx-1(:src="playProgress.image")
-  div
-    .flex
+  div.w-full
+    .flex.justify-between
       div
         p {{ playProgress.title }}
         p.text-sm.text-gray-400 {{ playProgress.artist }}
-      .flex.content-center.place-items-center.ml-3
+      .flex.content-center.place-items-center
         font-awesome-icon.mx-1(:icon="['fas', 'backward-step']")
         font-awesome-icon.mx-1.rounded-full.text-gray-800.bg-gray-100.p-1.w-4.h-4(:icon="['fas', playProgress.playing ? 'pause' : 'play']" @click="toggle")
         font-awesome-icon.mx-1(:icon="['fas', 'forward-step']")
--- aldify/src/views/Main.vue
+++ aldify/src/views/Main.vue
@@ -4,8 +4,8 @@
       button.text-gray-700.rounded-lg.py-1.px-3.mx-2.hover-bg-gray-100(:class="selectedIndex == 0 ? 'bg-gray-200' : ''" @click="select(0)") Songs
       button.text-gray-700.rounded-lg.py-1.px-3.mx-2.hover-bg-gray-100(:class="selectedIndex == 1 ? 'bg-gray-200' : ''" @click="select(1)") Albums
       button.text-gray-700.rounded-lg.py-1.px-3.mx-2.hover-bg-gray-100(:class="selectedIndex == 2 ? 'bg-gray-200' : ''" @click="select(2)") Artists
-  .mx-auto
-      c-song-card.my-3(v-for="song of songs" :song="song" @click="play(song)")
+  .mx-auto.flex.flex-wrap
+      c-song-card.mx-2.my-3(v-for="song of songs" :song="song" @click="play(song)")
 </template>
 
 <script lang="ts">
